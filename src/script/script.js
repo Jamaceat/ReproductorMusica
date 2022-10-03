c76b8d5603824e8ebff1d6104a27c0f576c0a60f
Johan <jamacea@uninorte.edu.co>
2022-10-03 10:56:39 -0500

    Proyecto avanzado, no comencé el git antes

---
+++ src/script/script.js
@@ -0,0 +1,8 @@
+const btn_subir = document.querySelector("#subirAudio")
+const formulario = document.querySelector("#subiendo")
+
+btn_subir.addEventListener("click", () => {
+	formulario.style.display = "flex"
+})
+
+console.log(formulario)
