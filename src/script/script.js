85b617d73cb46f9adb7df51cba1876d91baab84f
Johan <jamacea@uninorte.edu.co>
2022-10-03 12:12:25 -0500

    Pequeños cambios

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,15 @@
 const btn_subir = document.querySelector("#subirAudio")
 const formulario = document.querySelector("#subiendo")
+const cerrar = document.querySelector("#cancelar")
 
 btn_subir.addEventListener("click", () => {
 	formulario.style.display = "flex"
+	console.log(formulario.firstChild)
+})
+
+cerrar.addEventListener("click", (event) => {
+	event.preventDefault()
+	formulario.style.display = "none"
 })
 
 console.log(formulario)
